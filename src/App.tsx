f7cc6ed5f1321923e430d7353aea399429c77a37
farabek <farhodmuhamadiev4@gmail.com>
2025-08-16 20:48:46 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-800">
+        Tailwind + React + Vite + TS 🚀
+      </h1>
+    </div>
+  );
+}
